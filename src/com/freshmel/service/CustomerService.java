f0a24ef1ef669d3c6db270a6cae6669a475c4ba1
Shawn <584013293@qq.com>
2019-09-09 22:15:13 +1000

    add customer service

---
+++ src/com/freshmel/service/CustomerService.java
@@ -0,0 +1,24 @@
+package com.freshmel.service;
+
+import com.freshmel.dataMapper.CustomerMapper;
+import com.freshmel.model.Customer;
+
+import java.sql.SQLException;
+
+public class CustomerService {
+    public boolean register(Customer customer) throws SQLException {
+        // 1 check is the email in database
+        // 2 if not in database then insert to database
+        // 2 else return false
+
+        CustomerMapper customerMapper = new CustomerMapper();
+        return customerMapper.insert(customer);
+    }
+    public Customer login(Customer customer) throws SQLException {
+        // 1 check is the email and password in database correct
+        // 2 if correct return true
+        // 2 else return false
+        CustomerMapper customerMapper = new CustomerMapper();
+        return customerMapper.findByEmailANDPassword(customer);
+    }
+}
