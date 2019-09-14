9ef52d0db0cd3fb413ce9d7425f323c5a56b3e6d
Shawn <584013293@qq.com>
2019-09-14 22:30:12 +1000

    add filter for login

---
+++ src/com/freshmel/filter/LoginFilter.java
@@ -0,0 +1,43 @@
+package com.freshmel.filter;
+
+import com.freshmel.model.Customer;
+import com.freshmel.model.Vender;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebFilter(filterName="LoginFilter",urlPatterns={"/cart","/addCart","/addProduct","/deleteItemInCart","/deleteProduct","/logout","/productPhotoUpload","/updateAddress","/updateInfo","/updatePhoto","/updateProduct","/product.jsp","/cart.jsp"})
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        HttpSession session = httpRequest.getSession();
+        Vender vender = (Vender)session.getAttribute("vender");
+        Customer customer = (Customer) session.getAttribute("customer");
+        if((vender == null) && (customer == null)) {
+            httpRequest.setAttribute("info", "Please log in first!");
+            httpRequest.setAttribute("redirectURL", "index.jsp");
+            httpRequest.getRequestDispatcher("redirect.jsp").forward(servletRequest, servletResponse);
+            System.out.print("not log in but want to visit!");
+        }
+
+        // pass the request along the filter chain
+        filterChain.doFilter(httpRequest, httpResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
