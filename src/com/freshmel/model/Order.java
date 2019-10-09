7b4db6f6a152c56c0bdebae4fbdb46af705023b3
Shawn <584013293@qq.com>
2019-10-09 19:43:31 +1100

    add Order model

---
+++ src/com/freshmel/model/Order.java
@@ -0,0 +1,70 @@
+package com.freshmel.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class Order {
+    private Long id;
+    private List<OrderItem> orderItems;
+    private Timestamp createDate;
+    private Float totalPrice;
+    private Integer state;
+    private Long customerId;
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+
+    public Float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
